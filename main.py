e8bb91bac3f824dc81fcf2fe32df2a087ee3b173
Mateusz Jarliński <m.jarlinski@o2.pl>
2022-04-13 11:39:08 +0200

    Add main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from calendar import c
+from tkinter import N
+
+
 class address:
     def __init__(self, name, surname, company_name, position, adres):
         self.name = name
@@ -5,3 +9,6 @@ class address:
         self.company_name = company_name
         self.position = position
         self.adres = adres
+
+Flower = address(name="Jan", surname="Kowalski", company_name="Kwiatuszki",position="Dyrektor",adres="Kwiatuszki@o2.pl")
+Mug = address(name=)
