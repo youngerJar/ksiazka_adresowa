fd5719309a2dc8205692c78545d643d756f59abb
Mateusz Jarliński <m.jarlinski@o2.pl>
2022-04-13 11:34:30 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+class address:
+    def __init__(self, name, surname, company_name, position, adres):
+        self.name = name
+        self. surname = surname
+        self.company_name = company_name
+        self.position = position
+        self.adres = adres
