c91848bb790fcb6667c8b5e3d3c3a6f6eb5cb4d6
Mateusz Jarliński <m.jarlinski@o2.pl>
2022-04-23 18:42:40 +0200

    Add main.py

--- main.py
+++ main.py
@@ -11,11 +11,13 @@ class address:
         self.company_name = company_name
         self.position = position
         self.adres = adres
-        for name in self.name:
-            print(self.name,self.surname,",","adres korespodencyjny:",adres)
-            break
+        
+    def __str__(self):
+        return f'{self.name} {self.surname} {self.adres}'
 
 Flower = address(name="Jan", surname="Kowalski", company_name="Kwiatuszki",position="Dyrektor",adres="Kwiatuszki@o2.pl")
 Mug = address(name="Krzysztof", surname="Jerzyna", company_name="Jerzynka", position="Menager", adres="Jerzynki@io.pl")
 Desk= address(name="Sławomir",surname="Allworks", company_name="Desk For You", position="Dyrektor", adres="Deskforyou@o2.pl")
 Storczyk = address(name="Eugeniusz",surname="Kos", company_name="Storczyki",position="Menager",adres="storczyki@121.pl")
+all_firms = [Flower, Mug, Desk, Storczyk]
+print(all_firms)
