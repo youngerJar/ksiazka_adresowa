9eecfa396e1eba70f1d6906e5bf68ec4f999e3e3
Mateusz Jarliński <m.jarlinski@o2.pl>
2022-04-29 19:58:21 +0200

    Add main.py

--- main.py
+++ main.py
@@ -3,20 +3,30 @@ from faker import Faker
 fake = Faker("pl_PL")
 
 class card:
-    def __init__(self,first_name, last_name, email,tel_priv):
+    def __init__(self,first_name, last_name, email,tel_priv, position):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.tel_priv = tel_priv
+        self.position = position
+
+
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}, {self.email}"
 
     def __repr__(self):
         return f"card(first_name= {self.first_name} last_name= {self.last_name}, email={self.email}"
+
     def contact(self):
-        
-for firm in range(5):
-    print(fake.first_name(), fake.last_name(),fake.email())
+        return f"Kontaktuję się z: {self.first_name}"
+
+    def label_length(self):
+        return sum([len(self.first_name), len(self.last_name), 1])
 
 
+
+
+"""for firm in range(5):
+    print(fake.first_name(), fake.last_name(),fake.email(), fake.job()))
+"""
