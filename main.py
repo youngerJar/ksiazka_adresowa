c2778bc48eff6689953c17948a02ce2bfe8773f9
Mateusz Jarliński <m.jarlinski@o2.pl>
2022-04-30 20:24:42 +0200

    Add main.py

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from faker import Faker
 fake = Faker("pl_PL")
 
 class card:
-    def __init__(self,first_name, last_name, email,tel_priv):
+    def __init__(self,first_name, last_name, email, tel_priv,phone):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.tel_priv = tel_priv
+        self.phone = phone
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}, {self.email}"
@@ -22,16 +23,19 @@ class card:
         return sum([len(self.first_name), len(self.last_name), 1])
 
 class BusinessContact(card):
-    def __int__(self,position, company_name, tel_work,*args,**kwargs):
+    def __int__(self,position, company_name,*args,**kwargs):
         super().__init__(*args, **kwargs)
         self.position = position
-        self.company_name = company_name
-        self.tel_work = tel_work
+        self.company = company
+
+
 
     def workcontact(self):
-        return f"Wybieram numer firmowy : {self.tel_work} i dzwonię do {self.first_name} {self.last_name}"
+        return f"Wybieram numer firmowy : {self.phone} i dzwonię do {self.first_name} {self.last_name}"
 
-
-person1= card(first_name=fake.first_name(),last_name=fake.last_name(),email= fake.email(),tel_priv=fake.phone_number())
-
+person1 = BusinessContact(first_name=fake.first_name(), last_name=fake.last_name(),
+              email=fake.email(), tel_priv=fake.phone_number(),phone=fake.phone_number())
+print(fake.company())
 print(person1.contact())
+print(person1.workcontact())
+print(person1.label_length())
